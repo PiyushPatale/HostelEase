2025411fda6c3e1331a7754c267887b91a8dc9f6
PIYUSH PATALE <patale.bhausaheb21b@iiitg.ac.in>
2025-04-21 10:53:07 +0530

    Commit

--- src/components/api.js
+++ src/components/api.js
@@ -1,2 +1,2 @@
-// export const PUBLIC_SERVER_URL="https://hostelease-rbds.onrender.com"
-export const PUBLIC_SERVER_URL="http://localhost:5000"
+export const PUBLIC_SERVER_URL="https://hostelease-rbds.onrender.com"
+// export const PUBLIC_SERVER_URL="http://localhost:5000"
